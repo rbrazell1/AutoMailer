35b27da83a9ea0283a57603844e95a2afb49a8d7
rbrazell1 <brazellrussell@gmail.com>
2021-03-21 16:33:54 -0600

    Sends email on run

---
+++ src/main/java/SendMailSSL.java
@@ -0,0 +1,8 @@
+public class SendMailSSL {
+
+  public static void main(String[] args) {
+    //from,password,to,subject,message
+    Mailer.send("brazellrussell@gmail.com", "010406161SoniX!23", "brazellrussell@gmail.com", "hello javatpoint", "How r u?");
+    //change from, password and to
+  }
+}
